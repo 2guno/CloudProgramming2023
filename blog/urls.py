586231ce89c7aa086976b24ddac866a870db1ac2
LeeGunhooo <dldnldud123@naver.com>
2023-03-28 15:49:59 +0900

    CBV refactoring

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
+# urlconf for blog
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.PostList.as_view()),
+    path('<int:pk>/', views.PostDetail.as_view()),
 ]
