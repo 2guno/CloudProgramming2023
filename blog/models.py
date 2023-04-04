2762b2f02bc8ef6498b44f07dec04c3cacb61b83
LeeGunhooo <dldnldud123@naver.com>
2023-04-04 18:02:20 +0900

    태그 기능 추가

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,26 @@
-import os
+import os.path
 
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=20, unique=True)
+    slug = models.SlugField(max_length=50, unique=True, allow_unicode=True)
+
+    def get_absolute_url(self):
+        return f'/blog/tag/{self.slug}'
+
+    def __str__(self):
+        return self.name
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=50, unique=True)
-    slug = models.SlugField(max_length=200, allow_unicode=True)
+    name = models.CharField(max_length=20, unique=True)
+    slug = models.SlugField(max_length=50, unique=True, allow_unicode=True)
+
+    def get_absolute_url(self):
+        return f'/blog/category/{self.slug}'
 
     def __str__(self):
         return self.name
@@ -26,13 +40,14 @@ class Post(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
-    category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
+    category = models.ForeignKey(Category, blank=True, null=True, on_delete=models.SET_NULL)
+    tag = models.ManyToManyField(Tag)
 
     def __str__(self):
-        return f'[{self.pk}]{self.title} :: {self.author}'
+        return f'[{self.pk}] {self.title} - {self.author}'
 
     def get_absolute_url(self):
-        return f'/blog/{self.pk}/'
+        return f'/blog/{self.pk}'
 
     def get_file_name(self):
         return os.path.basename(self.file_upload.name)
